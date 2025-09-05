e5c5d65b915e476a0fb9f8dab538f1f29d89a230
jx393 <jx393@cornell.edu>
2025-09-05 15:16:19 -0400

    yay

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -13,16 +13,20 @@ module PairTripleDetector_GL
   input  wire in1,
   input  wire in2,
   output wire out
- );
 
-  //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // Implement pair/triple detector using explicit gate-level modeling
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+);
 
-  `ECE2300_UNUSED( in0 );
-  `ECE2300_UNUSED( in1 );
-  `ECE2300_UNUSED( in2 );
-  `ECE2300_UNDRIVEN( out );
+wire w;
+wire x;
+wire y;
+
+or(w, in0, in1);
+and(x, in0, in1);
+and(y, w, in2);
+or(out, y, x);
+
+
+
 
 endmodule
 
